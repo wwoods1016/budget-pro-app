198bfb5298816058df042098c5584c9ba331ad63
Will Woods <wwoods1016@gmail.com>
2019-05-14 13:53:14 -0400

    Server.js and ES6 tweaks

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,33 +1,27 @@
-// *********************************************************************************
-// html-routes.js - this file offers a set of routes for sending users to the various html pages
-// *********************************************************************************
-
 // Dependencies
-// =============================================================
-var path = require('path');
+const path = require('path');
 
 // Routes
-// =============================================================
-module.exports = function(app) {
+module.exports = app => {
 	// Each of the below routes just handles the HTML page that the user gets sent to.
 
 	// index route loads dashboard.html
-	app.get('/', function(req, res) {
+	app.get('/', (req, res) => {
 		res.sendFile(path.join(__dirname, '../public/dashboard.html'));
 	});
 
 	// household route loads household.html
-	app.get('/household', function(req, res) {
+	app.get('/household', (req, res) => {
 		res.sendFile(path.join(__dirname, '../public/household.html'));
 	});
 
 	// budget route loads budget.html
-	app.get('/budget', function(req, res) {
+	app.get('/budget', (req, res) => {
 		res.sendFile(path.join(__dirname, '../public/budget.html'));
 	});
 
 	// about route loads about-us.html
-	app.get('/about', function(req, res) {
+	app.get('/about', (req, res) => {
 		res.sendFile(path.join(__dirname, '../public/about-us.html'));
 	});
 };
