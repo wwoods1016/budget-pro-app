8b71eea7311dad533769c07273d6673d0aac0ead
Will Woods <wwoods1016@gmail.com>
2019-05-15 21:15:11 -0400

    Changes to dotenv

--- models/index.js
+++ models/index.js
@@ -6,7 +6,13 @@ const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
-const db = {};
+const db = require('db');
+
+db.connect({
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS
+});
 
 let sequelize;
 if (config.use_env_variable) {
