5a8b4fade7f819f262bb4f2e9503322cd460db66
Technoladie <38797497+t2the2ndpower@users.noreply.github.com>
2019-05-18 11:26:15 -0400

    db and seeds updates

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,21 +1,23 @@
+
+
 USE budgetPro_db;
 
 -- income-expense seeds
-INSERT INTO incm_exp_db (descript, IEtype,IEdate,amount,catagory,source) VALUE ("Puppy Purchase", "Expense","03/14/2019",250.00,"Wellness","bank account");
-INSERT INTO incm_exp_db (descript, IEtype,IEdate,amount,catagory,source) VALUE ("Weekly Groceries", "Expense","04/14/2019",300.00,"Food","bank account");
-INSERT INTO incm_exp_db (descript, IEtype,IEdate,amount,catagory,source) VALUE ("Paycheck", "Income","05/01/2019",7000.00,"Wellness","my good job");
-INSERT INTO incm_exp_db (descript, IEtype,IEdate,amount,catagory,source) VALUE ("Side Hustle", "Income","05/14/2019",2500.00,"Wellness","my awesome app");
-INSERT INTO incm_exp_db (descript, IEtype,IEdate,amount,catagory,source) VALUE ("Mortgage", "Expense","04/07/2019",5000.00,"Bills","bank account");
-INSERT INTO incm_exp_db (descript, IEtype,IEdate,amount,catagory,source) VALUE ("Car Note", "Expense","05/15/2019",350.00,"Bills","bank account");
-INSERT INTO incm_exp_db (descript, IEtype,IEdate,amount,catagory,source) VALUE ("Insurance", "Expense","05/14/2019",150.00,"Bills","bank account");
-INSERT INTO incm_exp_db (descript, IEtype,IEdate,amount,catagory,source) VALUE ("Stool App", "Expense","05/15/2019",11.99,"Wellness","bank account");
+INSERT INTO incm_exp (descript, IEtype,IEdate,amount,category,source) VALUE ("Puppy Purchase", "Expense","2019-03-14",250.00,"Wellness","bank account");
+INSERT INTO incm_exp (descript, IEtype,IEdate,amount,category,source) VALUE ("Weekly Groceries", "Expense","2019-04-14",300.00,"Food","bank account");
+INSERT INTO incm_exp (descript, IEtype,IEdate,amount,category,source) VALUE ("Paycheck", "Income","2019-05-01",7000.00,"Wellness","my good job");
+INSERT INTO incm_exp (descript, IEtype,IEdate,amount,category,source) VALUE ("Side Hustle", "Income","2019-04-15",2500.00,"Wellness","my awesome app");
+INSERT INTO incm_exp (descript, IEtype,IEdate,amount,category,source) VALUE ("Mortgage", "Expense","2019-04-07",5000.00,"Bills","bank account");
+INSERT INTO incm_exp (descript, IEtype,IEdate,amount,category,source) VALUE ("Car Note", "Expense","2019-04-15",350.00,"Bills","bank account");
+INSERT INTO incm_exp (descript, IEtype,IEdate,amount,category,source) VALUE ("Insurance", "Expense","2019-04-15",150.00,"Bills","bank account");
+INSERT INTO incm_exp (descript, IEtype,IEdate,amount,category,source) VALUE ("Stool App", "Expense","2019-05-15",11.99,"Wellness","bank account");
 
 
 -- Household seeds
-INSERT INTO household (hhName, relIeId) VALUE ("Thomas", "Expense","05/15/2019",11.99,"Wellness","bank account");
-INSERT INTO household (hhName, relIeId) VALUE ("Woods", "Expense","05/15/2019",11.99,"Wellness","bank account");
-INSERT INTO household (hhName, relIeId) VALUE ("Aujla", "Expense","05/15/2019",11.99,"Wellness","bank account");
-INSERT INTO household (hhName, relIeId) VALUE ("Reese", "Expense","05/15/2019",11.99,"Wellness","bank account");
+INSERT INTO household (hhName, relIeId) VALUE ("Thomas");
+INSERT INTO household (hhName, relIeId) VALUE ("Woods");
+INSERT INTO household (hhName, relIeId) VALUE ("Aujla");
+INSERT INTO household (hhName, relIeId) VALUE ("Reese");
 INSERT INTO household (hhName, relIeId) VALUE ("Deshpande");
 
 -- Household member seeds
@@ -28,11 +30,11 @@ INSERT INTO hhMember (hhMemFirstName, hhMemLastName,relHHId) VALUE ("Christoper"
 
 
 -- goals seeds
-INSERT INTO goals (gName, gDesc,gAmount,gDate) VALUE ("Trip to Bali", "Move the whole fam and the inlaws to BALI BABY!", 7000.00,"07/15/2019");
-INSERT INTO goals (gName, gDesc,gAmount,gDate) VALUE ("Trip to Bali", "Move the whole fam and the inlaws to BALI BABY!", 7000.00,"07/15/2019");
-INSERT INTO goals (gName, gDesc,gAmount,gDate) VALUE ("Trip to Bali", "Move the whole fam and the inlaws to BALI BABY!", 7000.00,"07/15/2019");
-INSERT INTO goals (gName, gDesc,gAmount,gDate) VALUE ("Trip to Bali", "Move the whole fam and the inlaws to BALI BABY!", 7000.00,"07/15/2019");
-INSERT INTO goals (gName, gDesc,gAmount,gDate) VALUE ("Trip to Bali", "Move the whole fam and the inlaws to BALI BABY!", 7000.00,"07/15/2019");
-INSERT INTO goals (gName, gDesc,gAmount,gDate) VALUE ("Trip to Bali", "Move the whole fam and the inlaws to BALI BABY!", 7000.00,"07/15/2019");
+INSERT INTO goals (gName, gDesc,gAmount,gDate) VALUE ("Trip to Bali", "Move the whole fam and the inlaws to BALI BABY!", 7000.00,"2020-06-01");
+INSERT INTO goals (gName, gDesc,gAmount,gDate) VALUE ("Retirement", "One Million Dollars and a house on the beach", 1000000.00,"2045-01-01");
+INSERT INTO goals (gName, gDesc,gAmount,gDate) VALUE ("College Fund", "Furman U!", 170000.00,"2023-08-15");
+INSERT INTO goals (gName, gDesc,gAmount,gDate) VALUE ("Christmas 2019", "Everyone gets a toy Coal Mine", 50000.00,"2019-11-15");
+INSERT INTO goals (gName, gDesc,gAmount,gDate) VALUE ("Commercial 3d Printer", "Maveric 3d Printer to print my own Tesla Parts", 85000.00,"2022-04-15");
+INSERT INTO goals (gName, gDesc,gAmount,gDate) VALUE ("Monster Truck Show", "Vroom Vrooom, Crush", 500.00,"2019-09-11");
 
 
