ef119e9eb6b9605bb562d043a7b20726e880444f
Technoladie <38797497+t2the2ndpower@users.noreply.github.com>
2019-05-20 19:57:53 -0400

    working demo on port 8080

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 // Dependencies
-const express = require('express');
+require('dotenv').config();
 
+const express = require('express');
 // Sets up the Express App
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 // Requiring our models for syncing
-const db = require('./models');
+const db = require('./models/index');
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
