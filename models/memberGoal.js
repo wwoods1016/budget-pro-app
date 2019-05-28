0eec15a7d78bdfb9a3239b6a32af1edbb5e137b6
Technoladie <38797497+t2the2ndpower@users.noreply.github.com>
2019-05-28 18:56:15 -0400

    income.js goals.js memberGoal.js households.js

---
+++ models/memberGoal.js
@@ -0,0 +1,36 @@
+module.exports = (sequelize, DataTypes) => {
+	const incm_exp = sequelize.define('incm_exp', {
+		amount: {
+			type: DataTypes.FLOAT,
+			allowNULL: false
+		},
+		IEdate: {
+			type: DataTypes.STRING,
+			allowNULL: false,
+			validate: {
+				'len': [1, 255]
+			}
+		},
+		category: {
+			type: DataTypes.STRING,
+			allowNULL: false,
+			validate: {
+				'len': [1, 255]
+			}
+		},
+		descript: {
+			type: DataTypes.TEXT,
+			allowNULL: false,
+			validate: {
+				'len': [1, 255]
+			}
+
+		},
+		source: {
+			type: DataTypes.STRING,
+			allowNULL: false
+		}
+	},{freezeTableName: true})
+	return incm_exp;
+
+};
--- public/js/income.js
+++ public/js/income.js
@@ -1,21 +1,83 @@
 // JS and JQuery for the income of an individual household will go within the function below.
-$(document).ready(() => {
-    $("income_submit_button").on("click", function (event) {
-        event.preventDefault();
-        var income;
+// $(document).ready(() => {
+//     $("income_submit_button").on("click", function (event) {
+//         event.preventDefault();
+//         var income;
 
-        var newIncome = {
-            source: $("#income-source").val().trim(),
-            category: $("#income-category").val().trim(),
-            amount: $("#income-amount").val().trim(),
-            date: $("#income-date").val().trim(),
-            description: $("income-description").val.trim()
-        };
-        console.log(newIncome);
+//         var newIncome = {
+//             source: $("#income-source").val().trim(),
+//             category: $("#income-category").val().trim(),
+//             amount: $("#income-amount").val().trim(),
+//             date: $("#income-date").val().trim(),
+//             description: $("income-description").val.trim()
+//         };
+//         console.log(newIncome);
 
 
-        // Adding event listeners for deleting, editing, and adding income
+//         // Adding event listeners for deleting, editing, and adding income
     
 
+//     });
+// });
+
+
+// JS and JQuery for the expenses overview page will go within the function below.
+$(document).ready(function () {
+    console.log("ready!");
+  });
+  
+  $("#income-form").on("submit", function (event) {
+    console.log("button has been click")
+    event.preventDefault();
+    var income;
+  
+    var newIncome = {
+      Source: $("#income-source").val().trim(),
+      Category: $("#income-category").val().trim(),
+      Amount: $("#income-amount").val().trim(),
+      Date: $("#income-date").val().trim(),
+      Description: $("#income-description").val().trim(),
+    };
+    // console.log(newIncome);
+  
+  
+    
+  
+  
+    $.post("/api/incm_exp", newIncome)
+      // On success, run the following code
+      .then(function (data) {
+        // Log the data we found
+        // console.log(data);
+        var row = $("<div>");
+        row.addClass("income");
+  
+        row.append("<p><strong> IncomeSource: </strong>" + newIncome.Source + "  </p>");
+        row.append("<p><strong> IncomeCategory: </strong>" + newIncome.Category + "</p>");
+        row.append("<p><strong> IncomeCategory: </strong>" + newIncome.Amount + "</p>");
+        //row.append("<p><strong> IncomeDescription: </strong>" + newIncome.expDescription + "</p>");
+
+        // $("#exp-section").append(row);  DO WE NEED AN INCOME SECTION???
+      });
+      $("#income-source").val("");
+      $("#income-category").val("");
+      $("#income-amount").val("");
+     // $("#income-description").val("")
+  
+  
+  
+    $.get("/api/incm_exp", function (data) {
+      console.log(data)
+  
+  
+  
     });
-});
+  
+  
+  
+  });
+  
+  
+  
+  
+  
