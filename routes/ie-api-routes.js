930bb7c44fcf8291b994a31ff57604348ef0d887
Smita Deshpande <smipra11@gmail.com>
2019-06-01 09:17:48 -0400

    files added

--- routes/ie-api-routes.js
+++ routes/ie-api-routes.js
@@ -5,28 +5,71 @@ const db = require('../models');
 
 // Routes
 module.exports = app => {
-  //Get route for all incomes and expenses
+  //Get route for all expenses
   app.get("/api/incm_exp", function (req, res) {
     //console.log(res)
     db.incm_exp.findAll({
-          })
+    })
+      .then(function (dbincm_exp) {
+        res.json(dbincm_exp);
+      });
+  });
+// Get Route for specific id
+  app.get("/api/incm_exp/:id", function (req, res) {
+    //console.log(res)
+    db.incm_exp.findAll({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(function (dbincm_exp) {
+        res.json(dbincm_exp);
+      });
+  });
+ //get route for perticular category
+  /*app.get("/api/incm_exp/category/:category", function (req, res) {
+    //console.log(res)
+    db.incm_exp.findAll({
+      where: {
+        category: req.params.category
+      }
+    })
+      .then(function (dbincm_exp) {
+        res.json(dbincm_exp);
+      });
+  });*/
+
+
+
+
+// get route for each category where amount is greate than $50
+  app.get("/api/incm_exp/category/:category", function (req, res) {
+    //console.log(res)
+    //const Op = Sequelize.Op
+
+    db.incm_exp.findAll({
+      attributes: ['id', [sequelize.fn('sum', sequelize.col('amount')), 'total']],
+      
+    })
       .then(function (dbincm_exp) {
         res.json(dbincm_exp);
       });
   });
 
-  app.get("/api/incm_exp/:IEType", function (req, res) {
+
+  //get rpute for IEType
+  app.get("/api/incm_exp/IEType/:IEType", function (req, res) {
     //console.log(res)
     db.incm_exp.findAll({
       where: {
-        IEType: req.body.IEType
+        IEType: req.params.IEType
       }
-          })
-      .then(function (dbincm_exp) {
-        res.json(dbincm_exp);
+    })
+      .then(function (result) {
+        res.json(result);
       });
   });
-  
+
   //Put route for updating income or expenses
   app.put("/api/incm_exp", function (req, res) {
     db.incm_exp.update(req.body,
@@ -52,6 +95,8 @@ module.exports = app => {
       });
   });
 
+
+
   //Post route for income and expenses comment
   app.post("/api/incm_exp", function (req, res) {
     db.incm_exp.create({
@@ -60,11 +105,10 @@ module.exports = app => {
       category: req.body.Category,
       descript: req.body.Description,
       source: req.body.Source,
-      IEType:req.body.IEType
-      
-          
-           
- 
+      IEType: req.body.IEType
+
+
+
     })
       .then(function (dbincm_exp) {
         res.json(dbincm_exp);
@@ -73,4 +117,6 @@ module.exports = app => {
         console.log(err);
       });
   });
- };
+
+  
+};
